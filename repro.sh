7abb1b383faddf7c57e05b6f111062ddce3042e3
Wieland Hoffmann <themineo@gmail.com>
2020-05-27 21:21:40 +0200

    Use the most recent setuptools

--- repro.sh
+++ repro.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+rm -rf venv
 python3 -m venv venv
 source venv/bin/activate
+pip install --upgrade pip setuptools
 python setup.py develop
 
 printf 'Run "source venv/bin/activate" and then pwbtestcli. It will not work\n'
